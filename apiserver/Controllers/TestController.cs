8fa1c0b52f4e41a278d24e154b85826c9b7410f2
r1ck7 <richims026@gmail.com>
2025-11-12 01:18:18 -0500

    Init - FOK CancellationToken+ILogger_Api+Wasm

---
+++ apiserver/Controllers/TestController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using shared.Mydata;
+
+namespace apiserver.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Test(
+            Person nperson,
+            ILogger<TestController> logger,
+            CancellationToken cancellationToken
+            )
+        {
+            try
+            {
+                if (nperson.Id > 0)
+                {
+                    await Task.Delay(2000, cancellationToken);
+                    var x = new
+                    {
+                        Cod = nperson.Id,
+                        Nombre = nperson.Nap + "_" + DateTime.Now.ToString()
+                    };
+                    logger.LogInformation("Consulta Api existosa!!!");
+                    return Ok(x);
+                }
+                else
+                {
+                    throw new ArgumentException("Id debe ser positivo",nameof(nperson));
+                }
+            }
+            catch (TaskCanceledException ex) when (ex.CancellationToken.IsCancellationRequested)
+            {
+                logger.LogWarning("Se cancelo la petición");
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ apiserver/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddCors(o => o.AddPolicy("AllowAll",
+    p => p.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader()));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+app.UseCors("AllowAll");
+app.UseHttpsRedirection();
+
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
